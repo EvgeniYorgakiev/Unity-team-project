bd98d460ad1c83819e91faae2090a75baf419ed8
mdamyanova <mdamyanova181@gmail.com>
2016-03-20 19:05:03 +0200

    reorganized assets

---
+++ Programmer's dream/Assets/Menu/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public Transform mainMenu;
+
+    public Transform optionsMenu;
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu(bool isClicked)
+    {
+        if (isClicked)
+        {
+            this.optionsMenu.gameObject.SetActive(isClicked);
+            this.mainMenu.gameObject.SetActive(false);
+        }
+        else
+        {
+            this.optionsMenu.gameObject.SetActive(isClicked);
+            this.mainMenu.gameObject.SetActive(true);
+        }
+    }
+}
