8fd6eb32171e9d5c4fe0b811866aaa412b1405a0
mdamyanova <mdamyanova181@gmail.com>
2016-03-29 14:42:10 +0300

    added resources

---
+++ Programmer's dream/Assets/Menu/Scripts/SoundsScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class SoundsScript : MonoBehaviour
+{
+    bool isMute;
+
+    public void Mute()
+    {
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+    }
+}
